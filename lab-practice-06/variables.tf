39076ca7ce138058c05e23090713f840d06ca8af
TIwase <tanu.z0uz0u@gmail.com>
2023-09-10 04:59:20 +0000

    直前コミットのコメントアウト箇所削除

--- lab-practice-06/variables.tf
+++ lab-practice-06/variables.tf
@@ -9,14 +9,3 @@ variable "instance_type_str" {}
 variable "root_volume_type_str" {}
 variable "root_volume_size_num" {}
 variable "instance_name_tag_str" {}
-# variable "tfc_aws_dynamic_credentials" {
-#   description = "Object containing AWS dynamic credentials configuration"
-#   type = object({
-#     default = object({
-#       shared_config_file = string
-#     })
-#     aliases = map(object({
-#       shared_config_file = string
-#     }))
-#   })
-# }
